e1d1031993e16da722695e7bdf6aef2b32f88ae6
msaltzma <msaltzman5@unl.edu>
2021-08-26 17:54:39 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Malcolm Saltzman and Brandon Le
+ *
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Malcolm Saltzman and Brandon Le\n");
+
+  return 0;
+}
